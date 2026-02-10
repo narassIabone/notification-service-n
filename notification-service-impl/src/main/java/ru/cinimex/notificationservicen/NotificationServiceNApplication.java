fb260b7c560a9a1e9a1ebd76f7ed3190e8840f1b
narasslabone <nail.galeev03@mail.ru>
2026-02-11 03:14:02 +0500

    Initial commit

---
+++ notification-service-impl/src/main/java/ru/cinimex/notificationservicen/NotificationServiceNApplication.java
@@ -0,0 +1,13 @@
+package ru.cinimex.notificationservicen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServiceNApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceNApplication.class, args);
+    }
+
+}
---
+++ notification-service-impl/src/test/java/ru/cinimex/notificationservicen/NotificationServiceNApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.cinimex.notificationservicen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceNApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
